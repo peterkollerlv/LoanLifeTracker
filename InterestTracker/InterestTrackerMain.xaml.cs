c908857c78041a47cff00eed9e4fd89bab67cd5f
Peter Koller <peterkollerlv@live.com>
2016-04-08 17:51:03 +0200

    Migrating to WPF

---
+++ InterestTracker/InterestTrackerMain.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterestTracker
+{
+    /// <summary>
+    /// Interaction logic for InterestTrackerMain.xaml
+    /// </summary>
+    public partial class InterestTrackerMain : Window
+    {
+        public InterestTrackerMain()
+        {
+            InitializeComponent();
+        }
+
+
+        //ui events
+
+        private void navLoanData_GotFocus(object sender, RoutedEventArgs e)
+        {
+            navMainTabControl.SelectedItem = navMainLoanData;
+        }
+
+        private void navMainLoanData_GotFocus(object sender, RoutedEventArgs e)
+        {
+            navTabControl.SelectedItem = navLoanData;
+        }
+    }
+}
---
+++ InterestTracker/Loan.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoanLifeTracker
+{
+    public class Loan
+    {
+        //all loan specific data stored in this class
+
+
+        //constructor for new loans
+
+        public Loan(bool newLoan)
+        {
+            LoanGuid = generateNewGuid();
+            LoanStartDate = DateTime.Now.Date;
+            LoanInterestRate = 5;
+            loanInterestPenaltyRate = 10;
+            LoanCurrency = "USD";
+            PaymentsList = new List<Payment>();
+        }
+
+        //constructor for existing loans
+
+        public Loan(Guid existingGuid)
+        {
+            LoanGuid = existingGuid;
+            PaymentsList = new List<Payment>();
+        }
+
+        //fields & properies
+
+        private Guid loanGuid;
+        public Guid LoanGuid
+        {
+            get
+            {
+                return loanGuid;
+            }
+            set
+            {
+                loanGuid = value;
+            }
+        }
+
+        private string loanTitle;
+        public string LoanTitle
+        {
+            get
+            {
+                return loanTitle;
+            }
+            set
+            {
+                loanTitle = value;
+            }
+        }
+
+        private string loanCompanyInfo;
+        public string LoanCompanyInfo
+        {
+            get
+            {
+                return loanCompanyInfo;
+            }
+            set
+            {
+                loanCompanyInfo = value;
+            }
+        }
+
+        private string loanLender;
+        public string LoanLender
+        {
+            get
+            {
+                return loanLender;
+            }
+            set
+            {
+                loanLender = value;
+            }
+        }
+
+        private string loanBeneficiary;
+        public string LoanBeneficiary
+        {
+            get
+            {
+                return loanBeneficiary;
+            }
+            set
+            {
+                loanBeneficiary = value;
+            }
+        }
+
+        private string loanCollectionAccount;
+        public string LoanCollectionAccount
+        {
+            get
+            {
+                return loanCollectionAccount;
+            }
+            set
+            {
+                loanCollectionAccount = value;
+            }
+        } 
+        
+        private DateTime loanStartDate;
+        public DateTime LoanStartDate
+        {
+            get
+            {
+                return loanStartDate.Date;
+            }
+            set
+            {
+                loanStartDate = value.Date;
+            }
+        }
+
+        private decimal loanInitialLoanAmount;
+        public decimal LoanInitialLoanAmount
+        {
+            get
+            {
+                return loanInitialLoanAmount;
+            }
+            set
+            {
+                loanInitialLoanAmount = value;
+            }
+        }
+
+        private string loanCurrency;
+        public string LoanCurrency
+        {
+            get
+            {
+                return loanCurrency;
+            }
+            set
+            {
+                loanCurrency = value;
+            }
+        }
+
+        private bool loanHasPenalty;
+        public bool LoanHasPenalty
+        {
+            get
+            {
+                return loanHasPenalty;
+            }
+
+            set
+            {
+                loanHasPenalty = value;
+            }
+        }
+
+
+        private DateTime loanInterestPenaltyDate;
+        public DateTime LoanInterestPenaltyDate
+        {
+            get
+            {
+                return loanInterestPenaltyDate;
+            }
+            set
+            {
+                loanInterestPenaltyDate = value;
+            }
+        }
+
+        private decimal loanInterestRate;
+        public decimal LoanInterestRate
+        {
+            get
+            {
+                return loanInterestRate;
+            }
+            set
+            {
+                loanInterestRate = value;
+            }
+        }
+
+        private decimal loanInterestPenaltyRate;
+        public decimal LoanInterestPenaltyRate
+        {
+            get
+            {
+                return loanInterestPenaltyRate;
+            }
+            set
+            {
+                loanInterestPenaltyRate = value;
+            }
+        }
+
+        private int loanInterestStructure;
+        public int LoanInterestStructure
+        {
+            get
+            {
+                return loanInterestStructure;
+            }
+            set
+            {
+                loanInterestStructure = value;
+            }
+        }
+
+        private List<Payment> paymentsList;
+        public List<Payment> PaymentsList
+        {
+            get
+            {
+                return paymentsList;
+            }
+            set
+            {
+                paymentsList = value;
+            }
+        }
+
+        //need to implement theses properties in LoanReportData:
+
+        private bool loanPaid;
+        public bool LoanPaid
+        {
+            get
+            {
+                return loanPaid;
+            }
+
+            set
+            {
+                loanPaid = value;
+            }
+        }
+
+        private bool loanSavedToDb;
+        public bool LoanSavedToDb
+        {
+            get
+            {
+                return loanSavedToDb;
+            }
+            set
+            {
+                loanSavedToDb = value;
+            }
+        }
+        
+        //loan methods
+
+
+        public Guid generateNewGuid()
+        {
+        Guid newGuid = Guid.NewGuid();  
+        return newGuid;          
+        }    
+    }
+}
